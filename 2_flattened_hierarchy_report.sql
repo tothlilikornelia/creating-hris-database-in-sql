1b63d7cfc9ddc7469568d1929d841a6d1194c6ac
Lili Kornélia Tóth <tothlilikornelia@gmail.com>
2026-07-26 19:41:41 +0200

    Update 2_flattened_hierarchy_report.sql

--- 2_flattened_hierarchy_report.sql
+++ 2_flattened_hierarchy_report.sql
@@ -78,31 +78,3 @@ Create INDEX idx_job_level ON hris_flattened_hierarchy_report(job_level);
 create INDEX idx_status ON hris_flattened_hierarchy_report(status);
 create INDEX idx_manager_id ON hris_flattened_hierarchy_report(manager_id);
 
-
-
---Create view to connect it with BI tool
-CREATE OR REPLACE VIEW hris_flattened_hierarchy_view AS
-SELECT
-    employee_id,
-    first_name || ' ' || last_name AS full_name,
-    job_title,
-    job_level,
-    department,
-    status,
-    manager_id,
-    manager_name,
-    manager_job_title,
-    manager_l2_id,
-    manager_l2_name,
-    manager_l2_job_title,
-    manager_l3_id,
-    manager_l3_name,
-    manager_l3_job_title,
-    manager_l4_id,
-    manager_l4_name,
-    manager_l4_job_title,
-    manager_l5_id,
-    manager_l5_name,
-    manager_l5_job_title
-
-    FROM hris_flattened_hierarchy_report;
